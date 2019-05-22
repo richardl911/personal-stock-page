7ccf930960e53e104fcae4ef4d9eaad4ff2a5998
Richard L <richard.698.lin@gmail.com>
2019-05-21 23:23:58 -0700

    Added a preliminary function to get stock info from quandl

--- web/js/stockViewer.js
+++ web/js/stockViewer.js
@@ -1,5 +1,5 @@
 let apiKey = config.apiKey || '';
-
+const urlBase = 'https://www.quandl.com/api/v3/datasets/WIKI/'
 
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -9,10 +9,45 @@ document.addEventListener('DOMContentLoaded', function() {
 function createFormListeners() {
   $('#apiKeyForm').on('submit', () => {
     event.preventDefault();
-    apiKey = $('#apiKeyForm > input[name="apiKey"]').val();
+//    apiKey = $('#apiKeyForm > input[name="apiKey"]').val();
+    checkValidKey();
   });
 }
 
-function getURL(dataSet) {
-// "https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=YOURAPIKEY"
+
+function checkValidKey(apiKey) {
+  getDataSet('FB')
+    .then((response, xhr) => {
+      console.log(response);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function getDataSet(stockSymbol) {
+  // ie : "https://www.quandl.com/api/v3/datasets/WIKI/FB/data.json?api_key=YOURAPIKEY"
+  let url = `${urlBase}${stockSymbol}/data.json?api_key=${apiKey}`
+  let xhr = new XMLHttpRequest();
+
+  return new Promise((resolve, reject) => {
+    xhr.onreadystatechange = function () {
+      if(xhr.readyState != 4) return;
+
+      if(xhr.status >= 200 && xhr.status < 300) resolve(xhr.response, xhr)
+      else reject({status: xhr.status, statusText: xhr.statusText});
+    };
+
+    xhr.open('GET', url);
+    xhr.send();
+
+  });
+}
+
+function getURLKey() {
+
+}
+
+function joinParameters() {
+
 }
